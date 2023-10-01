c6295c2dbad055de36b2c6006ee49bf8e9437295
vivaciouscoder <138902586+vivaciouscoder@users.noreply.github.com>
2023-10-01 23:17:17 +0300

    created forcast js

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ function showTemp(response) {
   );
 
   let updatedDate = document.querySelector("#date");
-  updatedDate.innerHTML = formatDate(response.data.time * 1000);
+  updatedDate.innerHTML = formatDate(response.data.time * 1000.0004);
 
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
@@ -90,6 +90,33 @@ function displayCelcious(event) {
   fahrenheitLink.classList.remove("active");
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["mon", "tue", "wed", "thu", "fri", "sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+       <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAm9JREFUaN7tmcFthDAQRbcESqAEl0ADkSiBY46UQAmUQAl0EErwdW+kA3fgjKNhNXJsGNvYyyqL9A/ZQPKf/Wc8JDet9e2V9dLm3wCXBAi57ve7AA2WxC3jdQoAGpcg7ZHMBZIMAMa6HeO2uksBgKEmwPwmcSUAGQGwXAIAc68jNYFW6zPz9QiqSgEMCQB7UqDWa/j7U5wFMGYC2NQ6zPeg7lUAFI2TWXmQBtXJAOYHY451ZvVovgJJ0JpcA5h9VcC83HYAjE+4+lM0AK66LGB8i49A8y2a13b+QwFKmX+c2CbvIEUA6iiAjC3TpZkUriTm16hzAKOjCplfSe4HYt6Zfy5AV3D1t9w3lnln/rkApeIzkJapHAD1lQEWkvvZYX6NnoUKACiS+95h/hcAa8KtJwM01qgQqkMAkdH8aI0KoeYl9xxYcowKJPdT5OoLLkCTIfe1Y1QIUX/aXyVyXTtwyynvxB9f9wrUhBqDZwSoZgC4oqXomRANAAYGkEZVTOMGWOEzIwNAHZ3IUQBm9Yh5zd0FuG8izywH5l1tdT7lpd78cmJk5eyAgbSg+wOA0RGdKhkAfnFrGWmZqy/JM5IRH/tcaKNGCUeGV18MzKqCZlDn+JwdOXyRoebH6Flop3A17STWzsyewjWaGKvfW3NQlQzgKNxhZ2dmT+EqTr1YE+n+bgUAzL7C9e0M9nt24ZK56DGoHd4fCdAyd0aEFK51+vLuDwDoEKIPaan4/dHAMAG201ecCuBpjVEtlXH69uz7E0aJ3ZYaaV7QQS03QONrqQkAg+/lPdcO/Cnc0ldqDTTcSfRpAO//1L8B/hnAD9B4AcpTDEFdAAAAAElFTkSuQmCC"
+          alt="rain"
+        />
+        <div class="weather-forecast-temp">
+          <span class="weather-forecast-temp-max"> 21° </span>
+          <span class="weather-forecast-temp-min"> 18° </span>
+        </div>
+      </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastHTML.innerHTML = forecastHTML;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 let celciousTemp = null;
 
 let form = document.querySelector("#searchForm");
@@ -102,3 +129,4 @@ let celciousLink = document.querySelector("#celcious-link");
 celciousLink.addEventListener("click", displayCelcious);
 
 search("Istanbul");
+displayForecast();
